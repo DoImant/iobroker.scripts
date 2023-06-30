d907fac643177aae24bd2ff58a54fc92fadc69c0
Kai R <k@pekaru.de>
2023-06-30 10:00:19 +0200

    datatypes changed

--- scripts/global/helper/roundTo.ts
+++ scripts/global/helper/roundTo.ts
@@ -1,7 +1,7 @@
 //
 // Rounds a number to "place" digits after the decimal point
 //
-function roundTo(n:any, place:any) {  
+function roundTo(n:number, place:number) {  
     let decimals = 10**place; 
     return (Math.round((n + Number.EPSILON) * decimals) / decimals);
 }
