910e209bb7cbc6a7e61c3e8ec88ae4b9326f7133
Kai R <k@pekaru.de>
2023-07-12 21:02:38 +0200

    made checkLedState() async

--- scripts/Server/swServerPowerLED.ts
+++ scripts/Server/swServerPowerLED.ts
@@ -1,7 +1,9 @@
 // 
-// Schalte die PowerLED des Rapberry Pi an oder aus.(Erfolgt über VIS Buttons)
-// Beim starten von ioBroker wird der zuletzt gespeicherte Status ausgelesen und
-// entsprechend gesetzt.
+// Switch the PowerLED of the Rapberry Pi on or off (via VIS buttons).
+// When ioBroker is started, the last saved status is read and set accordingly. 
+//
+// 21.06.2023 Initial version
+// 12.07.2023 Change to make the script work even if the option "Do not subscribe all states on start" is set.
 //
 function switchOn() {
   log('Schalte Raspberry Power-LED ein', 'info');
@@ -13,8 +15,9 @@ function switchOff() {
   exec('echo 0 | sudo tee /sys/class/leds/PWR/brightness');
 }
 
-function checkLedState() : void {
-  getState('0_userdata.0.Server.powerLed').val === 0 ? switchOff() : switchOn();
+async function checkLedState() : Promise<void> {
+  const res = await getStateAsync('0_userdata.0.Server.powerLed');
+  res.val === 0 ? switchOff() : switchOn();
 }
 
 // Check beim Start des Systems
