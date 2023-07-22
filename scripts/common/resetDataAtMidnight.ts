70fff632c27efa34948bd6a8840016cac23abb7e
Kai R <k@pekaru.de>
2023-07-23 00:16:20 +0200

    resetMinMax simplified

--- scripts/common/resetDataAtMidnight.ts
+++ scripts/common/resetDataAtMidnight.ts
@@ -5,28 +5,25 @@
 // 18.06.2023 Adapted to new data point structure for SensEgg sensors
 // 24.06.2023 Script renamed and some functions merged into this script. 
 // 09.07.2023 resetRainfall() id changed from rsd to rst.
+// 22.07.2023 resetMinMax simplified
 //
 
 //
 // At midnight, set the min/max settings to the current temperature values
 //
-function resetMinMaxT(id: string, i: number): void {
-  const devBaseString = '0_userdata.0.sensEgg.Devices.';
-  // Templatestring for all max, min IDs
-  const myRegEx = new RegExp("^.*(\\d{3})");
-  let rgxResult = myRegEx.exec(id);   // crop sensorID number from id
-  let ts: number = new Date().getTime();
-  if (rgxResult) {
-    let tMaxID = devBaseString + rgxResult[1] + ".tMax";
-    let tMinID = devBaseString + rgxResult[1] + ".tMin";
-    let tsMaxID = devBaseString + rgxResult[1] + '.tsMax'
-    let tsMinID = devBaseString + rgxResult[1] + '.tsMin'
-    if (existsState(tMaxID) && existsState(tMinID)) {
-      setState(tMaxID, getState(id).val, true);
-      setState(tMinID, getState(id).val, true);
-      setState(tsMaxID, ts, true);
-      setState(tsMinID, ts, true);
-    }
+function resetMinMaxT(id: string): void {
+  const devBaseString = id.replace(/ntcT$/,'');
+  const tMaxID = `${devBaseString}tMax`;
+  const tMinID = `${devBaseString}tMin`;
+  const tsMaxID = `${devBaseString}tsMax`;
+  const tsMinID = `${devBaseString}tsMin`;
+  const ts: number = new Date().getTime();
+
+  if (existsState(tMaxID) && existsState(tMinID)) {
+    setState(tMaxID, getState(id).val, true);
+    setState(tMinID, getState(id).val, true);
+    setState(tsMaxID, ts, true);
+    setState(tsMinID, ts, true);
   }
 }
 
